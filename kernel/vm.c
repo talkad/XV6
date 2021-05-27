ea7885a3fe9d7f25a8b9a5952cc4f05773fc41e9
talkad <talkad@post.bgu.ac.il>
2021-05-27 15:01:56 +0300

    love him

--- kernel/vm.c
+++ kernel/vm.c
@@ -175,7 +175,7 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
       panic("uvmunmap: walk");
-    if(((*pte & PTE_V) == 0))
+    if(((*pte & PTE_V) == 0) && (*pte & PTE_PG) == 0)
       panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
@@ -196,9 +196,10 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
       }
     }
 
+    #endif
+
     *pte = 0;
 
-    #endif
   }
 }
 
@@ -532,7 +533,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
-    if(((*pte & PTE_V) == 0))
+    if(((*pte & PTE_V) == 0) && (*pte & PTE_PG) == 0)
       panic("uvmcopy: page not present neither on memory nor disk");
 
     #ifndef NONE  
